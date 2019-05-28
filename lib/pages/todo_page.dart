77ce59b0418e47895c475d24945c420bf75c8683
Teerasej <teerasej@gmail.com>
2019-05-28 15:51:51 +0700

    เพิ่ม ListView แบบ Mockup

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class TodoPage extends StatelessWidget {
@@ -9,15 +8,29 @@ class TodoPage extends StatelessWidget {
         title: Text('To Do'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('27 May 2019'),
+                  subtitle: Text('Wake up 5 am'),
+                ),
+                ListTile(
+                  title: Text('25 May 2019'),
+                  subtitle: Text('Breakfast at canteen'),
+                ),
+                ListTile(
+                  subtitle: Text('Present C6 10 A.M.'),
+                )
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: RaisedButton(
-              onPressed: (){
-
-              },
+              onPressed: () {},
               child: Icon(Icons.add),
             ),
           )
